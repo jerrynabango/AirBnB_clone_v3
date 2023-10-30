93bad278a1af61c373014e21cf8ddd39695f24e1
jerrynabango <jerrynabango9@gmail.com>
2023-10-30 11:59:07 +0300

    instances

--- api/v1/app.py
+++ api/v1/app.py
@@ -4,19 +4,19 @@
 from flask_cors import CORS
 from flask import Flask, jsonify
 from .views import app_views
-from os import getenv
 from . import models
+import os
 
 
 app = Flask(__name__)
 
-CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
+CORS(app, resources=r"/*", origins=["0.0.0.0"])
 
 app.register_blueprint(app_views)
 
 
 @app.teardown_appcontext
-def teardown(e=None):
+def close_db(e=None):
     """Clean up the application context and exit."""
     models.storage.close()
 
@@ -24,9 +24,12 @@ def teardown(e=None):
 @app.errorhandler(404)
 def error(e):
     """Checks if the request is not found and returns a 404 error"""
-    status_code = str(e).split()[0]
-    message = e.description if "Not found" in e.description else "Not found"
-    return jsonify({"error": message}), status_code
+    code = str(e).split()[0]
+    result = e.description if "Not found" in e.description else "Not found"
+    return jsonify({"error": result}), code
+
 
 if __name__ == "__main__":
-    app.run(getenv("HBNB_API_HOST"), getenv("HBNB_API_PORT"))
+    host = os.getenv("HBNB_API_HOST", "0.0.0.0")
+    port = os.getenv("HBNB_API_PORT", 5000)
+    app.run(host=host, port=port, threaded=True)
