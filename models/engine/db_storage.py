498423fc99b6561edf16ef5fe499bf292db7b073
jerrynabango <jerrynabango9@gmail.com>
2023-10-30 11:36:57 +0300

    db storage

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -62,17 +62,6 @@ class DBStorage:
         if obj is not None:
             self.__session.delete(obj)
 
-    def reload(self):
-        """reloads data from the database"""
-        Base.metadata.create_all(self.__engine)
-        sess_factory = sessionmaker(bind=self.__engine, expire_on_commit=False)
-        Session = scoped_session(sess_factory)
-        self.__session = Session
-
-    def close(self):
-        """call remove() method on the private session attribute"""
-        self.__session.remove()
-
     def get(self, cls, id):
         """Gets the attribute associated with the given attribute"""
         if cls is not None:
@@ -86,3 +75,14 @@ class DBStorage:
             if cls is None or cls is classes[clss] or cls is clss:
                 count += self.__session.query(classes[clss]).count()
         return count
+
+    def reload(self):
+        """reloads data from the database"""
+        Base.metadata.create_all(self.__engine)
+        sess_factory = sessionmaker(bind=self.__engine, expire_on_commit=False)
+        Session = scoped_session(sess_factory)
+        self.__session = Session
+
+    def close(self):
+        """call remove() method on the private session attribute"""
+        self.__session.remove()
